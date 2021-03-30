829016707d9834beda2afe6ff1a6d1c06bae0f80
NitsanMaman <nit.maman@gmail.com>
2021-03-31 00:42:45 +0300

    add helloworld print on server

--- Client.py
+++ Client.py
@@ -1,6 +1,11 @@
-def main():
-	print("hey")
-    print("henlo")
-    print(":}")
+import socket
 
-main()
+HOST = '192.168.0.129'  # The server's hostname or IP address
+PORT = 7052        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
